5c6e2b766568d1de3b95e5b3edf2c0956fccc6bb
Carlin <carlin@MacBook-Air-de-Carlin.local>
2025-01-07 16:48:35 -0300

    start project my-portfolio with vite

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <div>
+      <nav className="">
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            Projetos
+          </li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default App
