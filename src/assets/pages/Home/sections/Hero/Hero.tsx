47580b0061225816c1d97f68dcd195818273f57d
Carlin <carlin@MacBook-Air-de-Carlin.local>
2025-01-08 17:23:08 -0300

    feat: page 1 finished

--- src/assets/pages/Home/sections/Hero/Hero.tsx
+++ src/assets/pages/Home/sections/Hero/Hero.tsx
@@ -13,35 +13,44 @@ function Hero() {
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
         display: "flex",
+        justifyContent: "center",
         alignItems: "center",
+        [theme.breakpoints.up('xs')] : {
+            backdropColor: "red",
+        },
+        [theme.breakpoints.up('md')] : {
+
+        }
     }));
 
     const StyledImg = styled("img")(({theme}) => ({
-        width: "80%",
+        width: "75%",
+        display: "flex",
+        margin: "10px auto",
         borderRadius: "50%",
-        border: `1px solid ${theme.palette.primary.contrastText}`
+        border: `1px solid ${theme.palette.primary.contrastText}`,
     }));
     
     return (
       <>
         <StyledHero>
             <Container maxWidth="lg">
-                <Grid container spacing={2}>
+                <Grid container display="flex" alignContent="center" spacing={2}>
                     <Grid item xs={12} md={5}>
                         <StyledImg src={Avatar} />
                     </Grid>
                     <Grid item xs={12} md={7}>
-                        <Typography  textAlign="center" color="secondary" variant="h1">Carlos Eufrásio</Typography>
+                        <Typography pt={3}  textAlign="center" color="secondary" variant="h1">Carlos Eufrásio</Typography>
                         <Typography textAlign="center" color="secondary" variant="h3" pb={3}>Desenvolvedor FrontEnd</Typography>
                          
                         <Grid container display={"flex"} justifyContent={"center"} spacing={3} pt={3}>
                             <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>    
-                                <StyledButton>
+                                <StyledButton onClick={() => console.log("Download")}>
                                     <FileDownloadIcon/>Download CV
                                 </StyledButton>                
                             </Grid>
                             <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
-                                <StyledButton>
+                                <StyledButton onClick={() => console.log("Contact")}>
                                     <MailOutlineIcon /> Contact me
                                 </StyledButton>
                             </Grid>
